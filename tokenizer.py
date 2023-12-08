479661b0b6adac3dcf5c03f13b4c6d5025cf1334
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2023-12-08 18:22:58 +0200

    add tokenizer 2

--- tokenizer.py
+++ tokenizer.py
@@ -1,44 +1,46 @@
-# from tokenizers import ByteLevelBPETokenizer
-# import textProcessing as tp
+from tokenizers import ByteLevelBPETokenizer
+import textProcessing as tp
+
+# Create and train a Byte Pair Encoding tokenizer
+tokenizer = ByteLevelBPETokenizer()
+# tokenizer.add_tokens()
+tokenizer.train(files="dataset/undiacritized_train_preprocessed.txt", vocab_size=50000, min_frequency=2, show_progress=True)
+
+# Save the trained tokenizer model
+tokenizer.save("arabic_bpe_tokenizer.json")
+
+# Encode a sample sentence
+encoded_sentence = tokenizer.encode(" قطع الأول يده إلخ  قال الزركشي")
+print("Encoded Sentence:", encoded_sentence.tokens)
+
+print("Token IDs:", encoded_sentence.ids)
+
+decoded_sentence = tokenizer.decode(encoded_sentence.ids)
+print("Decoded Sentence:", decoded_sentence)
+print("Decoded Sentence:", type(decoded_sentence))
+
+# decode id by id
+decoded_sentence = ""
+for id in encoded_sentence.ids:
+    decoded_sentence = tokenizer.decode([id])
+    print("Decoded Sentence:", decoded_sentence)
 
 
-# # Sample Arabic words
-# arabic_words = ["مرحبا", "كيف", "حالك", "اليوم"]
-# with open("arabic_words.txt", "w", encoding="utf-8") as f:
-#     for word in arabic_words:
-#         f.write(word + "\n")
+# import sentencepiece as spm
 
-# # Create and train a Byte Pair Encoding tokenizer
-# tokenizer = ByteLevelBPETokenizer()
-# tokenizer.add_tokens(list(tp.ARABIC_LETTERS))
-# tokenizer.train(files="dataset/train_preprocessed.txt", vocab_size=10000, min_frequency=5, show_progress=True)
+# # Sample sentence
+# sentence = "قوله"
 
-# # Save the trained tokenizer model
-# tokenizer.save("arabic_bpe_tokenizer.json")
+# # Train a SentencePiece model
+# spm.SentencePieceTrainer.train(input='dataset/undiacritized_train_preprocessed.txt',model_type='bpe', model_prefix='arabic_tokenizer', vocab_size=50000)
 
-# # Encode a sample sentence
-# encoded_sentence = tokenizer.encode("مرحبا كيف حالك اليوم ")
-# print("Encoded Sentence:", encoded_sentence.tokens)
-# print("Token IDs:", encoded_sentence.ids)
+# # Load the trained SentencePiece model
+# sp = spm.SentencePieceProcessor(model_file='arabic_tokenizer.model')
 
-# decoded_sentence = tokenizer.decode(encoded_sentence.ids)
-# print("Decoded Sentence:", decoded_sentence)
+# # Tokenize the sentence
+# tokens = sp.encode_as_pieces(sentence)
+# ids = sp.encode_as_ids(sentence)
 
-import sentencepiece as spm
-
-# Sample sentence
-sentence = "قوله"
-
-# Train a SentencePiece model
-spm.SentencePieceTrainer.train(input='dataset/undiacritized_train_preprocessed.txt',model_type='bpe', model_prefix='arabic_tokenizer', vocab_size=50000)
-
-# Load the trained SentencePiece model
-sp = spm.SentencePieceProcessor(model_file='arabic_tokenizer.model')
-
-# Tokenize the sentence
-tokens = sp.encode_as_pieces(sentence)
-ids = sp.encode_as_ids(sentence)
-
-# Print the results
-print("Tokens:", tokens)
-print("Token IDs:", ids)
+# # Print the results
+# print("Tokens:", tokens)
+# print("Token IDs:", ids)
--- utilities.py
+++ utilities.py
@@ -20,5 +20,33 @@ def create_tokenized_sentence(file_path,model_file):
     print(encoded_sentences[:10])
     return encoded_sentences
 
-create_tokenized_sentence("dataset/undiacritized_train_preprocessed.txt","arabic_tokenizer.model")
+def create_tokenized_sentence2(file_path,model_file):
+    # read text file sentence by sentence
+    sentences = []
+    with open(file_path,'rt', encoding='utf-8') as f:
+        text_line = f.readline().strip()
+        # train_text.append(train_text_line)
+        while(text_line != ""):
+            sentences.append(text_line)
+            text_line = f.readline().strip()
+    # apply encoding using sentencepiece
+    # Load the trained SentencePiece model
+    tokenizer = ByteLevelBPETokenizer(model_file)
+    # Encode
+    encoded_sentences = []
+    for sentence in sentences:
+        encoded_sentence = tokenizer.encode(sentence)
+        sentence_words = []
+        for id in encoded_sentence.ids:
+            decoded_word = tokenizer.decode([id])
+            # append only words
+            sentence_words.append(decoded_word)
+        encoded_sentences.append(sentence_words)
+            
+        
+    # print first 10 sentences
+    print(encoded_sentences[:10])
+    return encoded_sentences
+
+# create_tokenized_sentence("dataset/undiacritized_train_preprocessed.txt","arabic_tokenizer.model")
     
